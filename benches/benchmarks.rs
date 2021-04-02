39ce4a83d6d1811bcbeaaffa4bfee3ea5fcad1c2
Tyler <tyler@oxide.fi>
2021-04-02 07:53:38 -0500

    Added benchs
    Signed-off-by: Tyler <tyler@oxide.fi>

---
+++ benches/benchmarks.rs
@@ -0,0 +1,15 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use iqfeed_rs::models::Ops;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("parse", |b| {
+        b.iter(|| {
+            Ops::parse(black_box(
+                b"Q,GME,190.0000,1,16:40:18.814943,19,8346145,189.56,190,300,197,199.4600,187.1102,0.0,8717,O,",
+            ))
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use std::str::from_utf8;
 
 use lexical::parse;
+use once_cell::sync::Lazy;
 use rkyv::{Archive, Deserialize, Serialize};
 
 use crate::errors::ParsingError;
@@ -11,10 +12,28 @@ pub enum Ops {
     None,
 }
 
+static TIME_PARSER: Lazy<Vec<time::format_description::FormatItem>> =
+    Lazy::new(|| time::format_description::parse("[hour]:[minute]:[second].[subsecond digits:6]").unwrap());
+
 impl Ops {
     /// Parses a Vec<u8> into a valid `IQFeed` parsed message
     ///
     /// # Errors
+    ///
+    /// # Example
+    /// ```
+    /// # use iqfeed_rs::models::Ops;
+    ///
+    /// let src = b"Q,GME,190.0000,1,16:40:18.814943,19,8346145,189.56,190,300,197,199.4600,187.1102,0.0,8717,O,";
+    /// let op = Ops::parse(src).unwrap();
+    ///
+    /// match op {
+    ///     Ops::Trade(trade) => {
+    ///         assert_eq!(trade.symbol, "GME")
+    ///     },
+    ///     _ => {},
+    /// }
+    /// ```
     #[allow(clippy::match_on_vec_items)]
     pub fn parse(src: &[u8]) -> Result<Self, ParsingError> {
         let msg = from_utf8(src)?.split(',').collect::<Vec<_>>();
@@ -48,38 +67,17 @@ pub struct Trade {
 }
 
 impl Trade {
-    /// Parses comma delimited to `Trade`
-    ///
-    /// # Errors
-    /// If there is an issue parsing the CSV a `ParsingError` is returned
-    ///
-    /// # Panics
-    /// This will panic if there's an issue creating a format desc. Will be
-    /// removed at some point.
-    ///
-    /// # Example
-    /// ```
-    /// # use iqfeed_rs::models::Ops;
-    /// # let src = b"Q,GME,190.0000,1,16:40:18.814943,19,8346145,189.56,190.0000,300,197.500,199.4600.187.1102,O,8717";
-    /// let trade = Ops::parse(src).unwrap();
-    /// ```
-    pub fn parse(msg: &[&str]) -> Result<Self, ParsingError> {
-        // TODO: Move this to a oncecell maybe.
-        let tod = time::format_description::parse("[year].[month].[day] [hour]:[minute]:[second].[subsecond digits:9]")
-            .unwrap();
+    fn parse(msg: &[&str]) -> Result<Self, ParsingError> {
         let today = time::OffsetDateTime::now_utc();
 
         Ok(Self {
             symbol: msg[1].into(),
             most_recent_trade: parse(msg[2])?,
             most_recent_trade_size: parse(msg[3])?,
-            most_recent_trade_time: time::OffsetDateTime::parse(
-                &format!("{}.{}.{} {}000", today.year(), today.month(), today.day(), msg[4]),
-                &tod,
-            )
-            .unwrap()
-            .to_offset(time::UtcOffset::UTC)
-            .unix_timestamp_nanos(),
+            most_recent_trade_time: today
+                .replace_time(time::Time::parse(msg[4], &TIME_PARSER.as_ref())?)
+                .to_offset(time::UtcOffset::UTC)
+                .unix_timestamp_nanos(),
             most_recent_trade_market_center: parse(msg[5])?,
             total_volume: parse(msg[6])?,
             bid: match msg[7] {
